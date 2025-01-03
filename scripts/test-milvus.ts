af226d8e251d0cc2f07a343f22906c1bdbfde058
FabriiQ <kashif@fabriiq.com>
2025-01-03 15:18:53 +0500

    up

--- scripts/test-milvus.ts
+++ scripts/test-milvus.ts
@@ -1,11 +1,15 @@
+import 'dotenv/config';
 import { getMilvusClient } from '../lib/milvus/client';
 
 async function testConnection() {
+  console.log('Environment variables:');
+  console.log('MILVUS_ADDRESS:', process.env.MILVUS_ADDRESS);
+  console.log('MILVUS_TOKEN:', process.env.MILVUS_TOKEN);
+
   try {
     const client = await getMilvusClient();
     console.log('Testing connection...');
     
-    // Test basic operations
     const collections = await client.listCollections();
     console.log('Connected successfully! Available collections:', collections);
     
